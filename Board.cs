044b3c4cf57259d46e9ae9cbc69814e611dc2ba7
energy-power16 <121187713+energy-power16@users.noreply.github.com>
2024-01-15 19:18:02 +0300

    Add files via upload
    TicTacToe game

---
+++ Board.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tic_Tac_Toe
+{
+   
+class Board : IBoard
+    {
+        private char[,] board;
+        private int size;
+
+        public Board(int boardSize)
+        {
+            size = boardSize;
+            board = new char[size, size];
+            InitializeBoard();
+        }
+
+        private void InitializeBoard()
+        {
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    board[i, j] = ' ';
+                }
+            }
+        }
+
+        public void PrintBoard()
+        {
+            Console.Clear();
+            Console.WriteLine("Current Board:");
+
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    Console.Write(board[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public bool IsValidMove(int row, int col)
+        {
+            return row >= 0 && row < size && col >= 0 && col < size && board[row, col] == ' ';
+        }
+
+        public void MakeMove(int row, int col, char playerSymbol)
+        {
+            board[row, col] = playerSymbol;
+        }
+
+        public bool CheckWinner(char playerSymbol)
+        {
+            for (int i = 0; i < size; i++)
+            {
+                if (CheckRow(i, playerSymbol) || CheckColumn(i, playerSymbol))
+                    return true;
+            }
+
+            return CheckDiagonals(playerSymbol);
+        }
+
+        private bool CheckRow(int row, char playerSymbol)
+        {
+            for (int i = 0; i < size; i++)
+            {
+                if (board[row, i] != playerSymbol)
+                    return false;
+            }
+            return true;
+        }
+
+        private bool CheckColumn(int col, char playerSymbol)
+        {
+            for (int i = 0; i < size; i++)
+            {
+                if (board[i, col] != playerSymbol)
+                    return false;
+            }
+            return true;
+        }
+
+        private bool CheckDiagonals(char playerSymbol)
+        {
+            bool leftDiagonal = true;
+            bool rightDiagonal = true;
+
+            for (int i = 0; i < size; i++)
+            {
+                leftDiagonal &= (board[i, i] == playerSymbol);
+                rightDiagonal &= (board[i, size - 1 - i] == playerSymbol);
+            }
+
+            return leftDiagonal || rightDiagonal;
+        }
+    }
+}
---
+++ Session.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tic_Tac_Toe
+{
+    class Session : ISession
+    {
+        private Board board;
+        private int boardSize;
+        private char currentPlayer;
+        private char player1Symbol;
+        private char player2Symbol;
+
+        public Session(int boardSize, char player1Symbol, char player2Symbol)
+        {
+            this.boardSize = boardSize;
+            board = new Board(boardSize);
+            this.player1Symbol = player1Symbol;
+            this.player2Symbol = player2Symbol;
+            currentPlayer = player1Symbol;
+        }
+
+        public void StartGame()
+        {
+            do
+            {
+                board.PrintBoard();
+                MakeMove();
+                SwitchPlayer();
+            } while (!CheckGameOver());
+
+            board.PrintBoard();
+            Console.WriteLine($"Player {currentPlayer} wins!");
+        }
+
+        private void MakeMove()
+        {
+            int row, col;
+            do
+            {
+                Console.WriteLine($"Player {currentPlayer}, enter your move (row and column, separated by space):");
+                string[] input = Console.ReadLine().Split(' ');
+
+                if (input.Length != 2 || !int.TryParse(input[0], out row) || !int.TryParse(input[1], out col))
+                {
+                    Console.WriteLine("Invalid input. Try again.");
+                    continue;
+                }
+
+                if (!board.IsValidMove(row, col))
+                {
+                    Console.WriteLine("Invalid move. Try again.");
+                    continue;
+                }
+
+                board.MakeMove(row, col, currentPlayer);
+                break;
+            } while (true);
+        }
+
+        private void SwitchPlayer()
+        {
+            currentPlayer = (currentPlayer == player1Symbol) ? player2Symbol : player1Symbol;
+        }
+
+        private bool CheckGameOver()
+        {
+            if (board.CheckWinner(player1Symbol) || board.CheckWinner(player2Symbol))
+                return true;
+
+            // Check for a tie
+            for (int i = 0; i < boardSize; i++)
+            {
+                for (int j = 0; j < boardSize; j++)
+                {
+                    if (board.IsValidMove(i, j))
+                        return false;
+                }
+            }
+
+            Console.WriteLine("It's a tie!");
+            return true;
+        }
+    }
+}
