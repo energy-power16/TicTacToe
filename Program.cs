e7bced4054e20d837672d243b4739fff52d56362
energy-power16 <121187713+energy-power16@users.noreply.github.com>
2024-01-16 15:46:56 +0300

    Timed session edition

--- Program.cs
+++ Program.cs
@@ -14,7 +14,13 @@ class Program
         Console.Write("Enter player 2 symbol: ");
         char player2Symbol = char.Parse(Console.ReadLine());
 
-        Session ticTacToeSession = new Session(boardSize, player1Symbol, player2Symbol);
+        Console.Write("Enter player 1 game time in seconds: ");
+        int player1TimeInSeconds = int.Parse(Console.ReadLine());
+
+        Console.Write("Enter player 2 game time in seconds: ");
+        int player2TimeInSeconds = int.Parse(Console.ReadLine());
+
+        TimedSession ticTacToeSession = new TimedSession(boardSize, player1Symbol, player2Symbol, player1TimeInSeconds, player2TimeInSeconds);
         ticTacToeSession.StartGame();
     }
 }
